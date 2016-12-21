b502d22b45ef5870f44cbd5cf27479b0fff522f6
Alex Utephov <alexpark400@gmail.com>
2016-12-21 20:32:37 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<title>Prog</title>
+
+	<meta charset="UTF-8"/>
+
+	<link rel="stylesheet" href="media/css/style-forum.css">
+	<link rel="stylesheet" type="text/css" href="media/assets/font-awesome-4.6.3/css/font-awesome.min.css">
+
+</head>
+
+<body>
+	
+	<div class="wrapper">
+
+		<header>
+
+			<div class="container">
+
+				<nav class="top-menu">
+					
+					<ul>
+						<li><a href="#">Публикация</a></li>
+						<li><a href="#">Статьй</a></li>
+						<li><a href="#">Публикаций</a></li>
+						<li><a href="#">Учебник</a></li>
+						<li><a href="#">Вход</a></li>
+						<li><a href="#">Регистрация</a></li>
+					</ul>
+
+				</nav>
+
+			</div>
+
+		</header>
+
+		<section>
+
+			<div class="container">
+
+				<div class="meddle-menu clearfix">
+
+					<ul>
+						<li><a href="#">Профиль</a></li>
+						<li><a href="#">Блог</a></li>
+						<li><a href="#">Вакансии</a></li>
+						<li><a href="#">Подписчики</a></li>
+					</ul>
+
+				</div>
+
+				
+
+				<div class="content clearfix">
+					
+					<div class="posts_lists">
+						
+						<div class="post_hader">
+							
+							<h1>Какой-то пост!111111111111111111111111111<br/>11111111111111111111111111111111111111111<br>1111111111111111111</h1>
+
+						</div>
+
+						<div class="posts_content">
+
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dicta accusamus similique illo quos odio quo alias hic ad fuga deserunt dolorem enim assumenda, modi optio quas veritatis fugiat suscipit nesciunt unde.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, quidem recusandae neque. Laborum cumque, accusantium! Fugit at ab explicabo suscipit tenetur non facere iure quas quaerat consequatur molestias perspiciatis expedita quod, quis, praesentium vitae doloribus repudiandae doloremque esse. Culpa, facilis libero unde perspiciatis quasi.
+
+						</div>
+
+					</div><hr>
+
+					<div class="posts_lists">
+						
+						<div class="post_hader">
+							
+							<h1>Какой-то пост!11</h1>
+
+						</div>
+
+						<div class="posts_content">
+
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dicta accusamus similique illo quos odio quo alias hic ad fuga deserunt dolorem enim assumenda, modi optio quas veritatis fugiat suscipit nesciunt unde.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, quidem recusandae neque. Laborum cumque, accusantium! Fugit at ab explicabo suscipit tenetur non facere iure quas quaerat consequatur molestias perspiciatis expedita quod, quis, praesentium vitae doloribus repudiandae doloremque esse. Culpa, facilis libero unde perspiciatis quasi.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, laudantium.
+
+						</div>
+
+					</div><hr>
+
+					<div class="posts_lists">
+						
+						<div class="post_hader">
+							
+							<h1>Какой-то пост!111111111111111111111111111<br/>11111111111111111111111111111111111111111<br>1111111111111111111</h1>
+
+						</div>
+
+						<div class="posts_content">
+
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dicta accusamus similique illo quos odio quo alias hic ad fuga deserunt dolorem enim assumenda, modi optio quas veritatis fugiat suscipit nesciunt unde.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, quidem recusandae neque. Laborum cumque, accusantium! Fugit at ab explicabo suscipit tenetur non facere iure quas quaerat consequatur molestias perspiciatis expedita quod, quis, praesentium vitae doloribus repudiandae doloremque esse. Culpa, facilis libero unde perspiciatis quasi.
+
+						</div>
+
+					</div><hr>
+
+					<div class="posts_lists">
+						
+						<div class="post_hader">
+							
+							<h1>Какой-то пост!111111111111111111111111111<br/>11111111111111111111111111111111111111111<br>1111111111111111111</h1>
+
+						</div>
+
+						<div class="posts_content">
+
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dicta accusamus similique illo quos odio quo alias hic ad fuga deserunt dolorem enim assumenda, modi optio quas veritatis fugiat suscipit nesciunt unde.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, quidem recusandae neque. Laborum cumque, accusantium! Fugit at ab explicabo suscipit tenetur non facere iure quas quaerat consequatur molestias perspiciatis expedita quod, quis, praesentium vitae doloribus repudiandae doloremque esse. Culpa, facilis libero unde perspiciatis quasi.
+
+						</div>
+
+					</div><hr>
+
+					<div class="posts_lists">
+						
+						<div class="post_hader">
+							
+							<h1>Какой-то пост!111111111111111111111111111<br/>11111111111111111111111111111111111111111<br>1111111111111111111</h1>
+
+						</div>
+
+						<div class="posts_content">
+
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dicta accusamus similique illo quos odio quo alias hic ad fuga deserunt dolorem enim assumenda, modi optio quas veritatis fugiat suscipit nesciunt unde.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, quidem recusandae neque. Laborum cumque, accusantium! Fugit at ab explicabo suscipit tenetur non facere iure quas quaerat consequatur molestias perspiciatis expedita quod, quis, praesentium vitae doloribus repudiandae doloremque esse. Culpa, facilis libero unde perspiciatis quasi.
+
+						</div>
+
+					</div>
+
+				</div>
+
+				<div class="sidebar clearfix">
+
+					<div class="sidebar-content">
+						
+						<h2 class="news">Новости</h2>
+
+						<p><a href="#">Организация пространства типового центра разработки.</a></p>
+
+						<p class="s-content">28 октября 2016</p><hr>
+
+						<p><a href="#">Организация пространства типового центра разработки.</a></p>
+
+						<p class="s-content">28 октября 2016</p><hr>
+
+						<p><a href="#">Организация пространства типового центра разработки.</a></p>
+
+						<p class="s-content">28 октября 2016</p>
+
+						<p><a href="#">Организация пространства типового центра разработки.</a></p>
+
+						<p class="s-content">28 октября 2016</p>
+
+						<p><a href="#">Организация пространства типового центра разработки.</a></p>
+
+						<p class="s-content">28 октября 2016</p>
+
+						<p><a href="#">Организация пространства типового центра разработки.</a></p>
+
+						<p class="s-content">28 октября 2016</p>
+
+					</div>
+
+				</div>
+
+				<div class="sidebar c-sidebar clearfix">
+
+					<div class="sidebar-content">
+						
+						
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="clear"></div>
+
+			<div class="catPages1" align="center" id="pagesBlock1" style="clear:both;"> 
+				<b class="swchItemA"><span>1</span></b> 
+				<a class="swchItem" href="#"><span>2</span></a>
+				<a class="swchItem" href="#"><span>3</span></a>
+				<a class="swchItem" href="#" onclick="spages('2');return false;"><span>4</span></a>
+				<a class="swchItem" href="#" onclick="spages('2');return false;"><span>5</span></a>
+				<a class="swchItem" href="#" onclick="spages('2');return false;"><span>...</span></a>
+				<a class="swchItem" href="#" onclick="spages('2');return false;"><span><i class="fa fa-angle-double-right" aria-hidden="true"></i></span></a>
+			</div> 
+
+		</section>
+		
+		
+		
+
+		<footer>
+				
+				<div class="footer-image"><img src="/image/blog.png" alt="/" width="375"></div>
+
+				<nav class="bottom-menu">
+					
+					<ul>
+						<li><a href="#">Главная</a></li>
+						<li><a href="#">О бо мне</a></li>
+						<li><a href="#">Я в вконтакте</a></li>
+						<li><a href="#">Владелец</a></li>
+					</ul>
+
+				</nav>
+
+		</footer>
+
+	</div>
+	<script src="media/assets/jQuery/jquery-3.1.1.min.js" defer></script>
+	<script src="media/js/common.js" defer></script>
+</body>
+</html>
